b5efa6ea66065ddb84cc92c5ecc71480a592b403
Rajdeep <chahal.rajdeep03@gmail.com>
2019-10-10 19:49:37 -0400

    example 1

---
+++ day3_swift/Faculty.swift
@@ -0,0 +1,16 @@
+//
+//  Faculty.swift
+//  day3_swift
+//
+//  Created by MacStudent on 2019-10-10.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Faculty
+{
+    var facultyID: Int?
+    var facultyName:  String?
+    
+    //?, = 0 , ! , ?? disable error
+}
---
+++ day3_swift/StudentStructure.swift
@@ -0,0 +1,13 @@
+//
+//  StudentStructure.swift
+//  day3_swift
+//
+//  Created by MacStudent on 2019-10-10.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+struct Student
+{
+    var StudentID: Int
+}
